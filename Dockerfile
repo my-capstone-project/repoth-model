efc9de5a1252f674ac25ed855c6d5787766cff11
Maskumambang Padmanegara <43704863+Dimaspadma@users.noreply.github.com>
2023-06-05 01:17:15 +0700

    Dev (#1)
    * test trigger
    
    * update project_id env
    
    * tensorflow
    
    * classes result
    
    * update str classes
    
    * Update Dockerfile
    
    * Update server.py
    
    * add storage upload
    
    * change request to image
    
    * change variable image
    
    ---------
    
    Co-authored-by: Dimaspadma <Dimaspadma@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,18 @@
-FROM google/cloud-sdk:alpine as bucket
-ARG KEY_FILE
-ARG GCLOUD_PROJECT_STG
-ARG GCLOUD_BUCKET
-ARG VERSION
-WORKDIR /models
-RUN echo ${KEY_FILE} | base64 -d > gcloud.json
-RUN gcloud auth activate-service-account --key-file gcloud.json
-RUN gcloud config set project ${GCLOUD_PROJECT_STG}
-RUN gsutil -m cp -r ${GCLOUD_BUCKET}/${VERSION}/model.h5 /models
+# FROM google/cloud-sdk:alpine as bucket
+# ARG KEY_FILE
+# ARG GCLOUD_PROJECT_STG
+# ARG GCLOUD_BUCKET
+# ARG VERSION
+# WORKDIR /models
 
-# FROM tensorflow/tensorflow
-# RUN pip install Pillow fastapi uvicorn[standard] python-multipart
-# WORKDIR /app
-# COPY . ./
+# RUN echo ${KEY_FILE} | base64 -d > gcloud.json
+# RUN gcloud auth activate-service-account --key-file gcloud.json
+# RUN gcloud config set project ${GCLOUD_PROJECT_STG}
+# RUN gsutil -m cp -r ${GCLOUD_BUCKET}/${VERSION}/model.h5 /models
+
+FROM tensorflow/tensorflow
+RUN pip install Pillow fastapi uvicorn[standard] python-multipart google-cloud-storage
+WORKDIR /app
+COPY . ./
 # COPY --from=bucket /models .
-# CMD ["uvicorn", "server:app", "--reload", "--host", "0.0.0.0"]
+CMD ["uvicorn", "server:app", "--reload", "--host", "0.0.0.0"]
--- server.py
+++ server.py
@@ -2,32 +2,73 @@ from fastapi import FastAPI, File, UploadFile
 from typing import Union
 from typing_extensions import Annotated
 from PIL import Image
+import hashlib
+import os
+import pathlib
 
 from model import *
+from storage import *
+
 
 app = FastAPI()
+
+DB = load_model()
+
+BUCKET = authenticate_implicit_with_adc()
 
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
 
 
-@app.post("/predict/")
-async def create_upload_file(file: UploadFile):
+@app.post("/predict")
+async def create_upload_file(image: UploadFile):
+  
+  # Hash file name
+  filename = image.filename
+  hash_filename = hashlib.md5(filename.encode()).hexdigest() + pathlib.Path(filename).suffix
   
   # Write image
-  with open('image.jpg','wb') as image:
-    content = await file.read()
-    image.write(content)
-    image.close()
+  with open(hash_filename, 'wb') as img:
+    content = await image.read()
+    img.write(content)
+    img.close()
   
-  classes = predict(load_model(), "image.jpg")
+  # Predict Image
+  try:
+    classes = predict(DB, hash_filename)
+  except:
+    # Error
+    return {
+      "error": True,
+      "message": "Image is not ok"
+    }
 
   print(classes[0])
 
   if classes[0]>0.5:
-    status = "normal"
+    pothole = False
   else:
-    status = "pothole"
+    pothole = True
   
-  return {"filename": file.filename, "status": status}
+  url = ""
+  if pothole:
+    # Uload image to Storage Bucket
+    print("Upload bucket")
+    bucket_upload_image(BUCKET, hash_filename)
+    url = "https://storage.googleapis.com/potholeimages/" + hash_filename
+    print("Success url:", url)
+  
+  # Remove image
+  os.remove(hash_filename)
+
+  return {
+    "error": False,
+    "message": "Success Predict",
+    "result": {
+      "filename": filename,
+      "pothole": pothole,
+      "url": url
+    }
+  }
+
